e98745b8aa809acee9e0f4e5944fab14e2b2e2ca
Roman Mogilatov <roman.mogilatov@cupid.com>
2015-01-05 11:37:57 +0200

    removing wheel upload from register command in setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ with open('VERSION') as version:
 # Helper commands.
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
     print('You probably want to also tag the version now:')
     print('  git tag -a %s -m \'version %s\'' % (version, version))
     print('  git push --tags')
