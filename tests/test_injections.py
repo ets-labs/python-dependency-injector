54f35a5d351a70f1b34dacd631542b79a33d45a5
Roman Mogilatov <roman.mogilatov@cupid.com>
2015-03-11 00:23:26 +0200

    adding some tests

--- tests/test_injections.py
+++ tests/test_injections.py
@@ -11,6 +11,11 @@ class InjectionTest(unittest.TestCase):
     def test_init(self):
         """Test Injection creation and initialization."""
         injection = Injection('some_arg_name', 'some_value')
-
         self.assertEqual(injection.name, 'some_arg_name')
         self.assertEqual(injection.injectable, 'some_value')
+
+    def test_value_with_scalar_injectable(self):
+        """Test Injection value property."""
+        injection = Injection('some_arg_name', 'some_value')
+        self.assertEqual(injection.value, 'some_value')
+
