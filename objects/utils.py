e817f726c20ccf40cd43d4e6cf36235187a27c20
Roman Mogilatov <roman.mogilatov@cupid.com>
2015-03-18 12:57:12 +0200

    Fix of bug in Python 2.6 with failed isclass check in inspect module

--- objects/utils.py
+++ objects/utils.py
@@ -1,13 +1,13 @@
 """Utils module."""
 
-from inspect import isclass
+from six import class_types
 
 from .errors import Error
 
 
 def is_provider(instance):
     """Check if instance is provider instance."""
-    return (not isclass(instance) and
+    return (not isinstance(instance, class_types) and
             hasattr(instance, '__IS_OBJECTS_PROVIDER__'))
 
 
@@ -21,23 +21,23 @@ def ensure_is_provider(instance):
 
 def is_injection(instance):
     """Check if instance is injection instance."""
-    return (not isclass(instance) and
+    return (not isinstance(instance, class_types) and
             hasattr(instance, '__IS_OBJECTS_INJECTION__'))
 
 
 def is_init_arg_injection(instance):
     """Check if instance is init arg injection instance."""
-    return (not isclass(instance) and
+    return (not isinstance(instance, class_types) and
             hasattr(instance, '__IS_OBJECTS_INIT_ARG_INJECTION__'))
 
 
 def is_attribute_injection(instance):
     """Check if instance is attribute injection instance."""
-    return (not isclass(instance) and
+    return (not isinstance(instance, class_types) and
             hasattr(instance, '__IS_OBJECTS_ATTRIBUTE_INJECTION__'))
 
 
 def is_method_injection(instance):
     """Check if instance is method injection instance."""
-    return (not isclass(instance) and
+    return (not isinstance(instance, class_types) and
             hasattr(instance, '__IS_OBJECTS_METHOD_INJECTION__'))
