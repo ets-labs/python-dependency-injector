e8c3b6a620addc9456df55731728f0cd14e04536
Roman Mogilatov <roman.mogilatov@cupid.com>
2015-03-23 16:40:59 +0200

    Removing of old check function from manage.py

--- manage.py
+++ manage.py
@@ -23,16 +23,5 @@ def tag():
     os.system('git push --tags')
 
 
-@manager.command
-def check():
-    """Check `objects` library and examples with code analyzers."""
-    os.system('pylint objects/ --rcfile=./.pylintrc')
-    os.system('flake8 objects/')
-    os.system('pep257 objects/')
-
-    os.system('flake8 examples/')
-    os.system('pep257 examples/')
-
-
 if __name__ == '__main__':
     manager.main()
