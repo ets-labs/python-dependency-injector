dbc368d5f621898d06b3d717165c3e27c309d563
Roman Mogilatov <roman.mogilatov@cupid.com>
2015-06-18 14:55:13 +0300

    Minor updates for static provider docs

--- examples/providers/static.py
+++ examples/providers/static.py
@@ -6,18 +6,18 @@ from objects.providers import Function
 from objects.providers import Value
 
 
-# Provides class - `object`
+# Provides class - `object`:
 cls_provider = Class(object)
 assert cls_provider() is object
 
-# Provides object - `object()`
+# Provides object - `object()`:
 object_provider = Object(object())
 assert isinstance(object_provider(), object)
 
-# Provides function - `len`
+# Provides function - `len`:
 function_provider = Function(len)
 assert function_provider() is len
 
-# Provides value - `123`
+# Provides value - `123`:
 value_provider = Value(123)
 assert value_provider() == 123
