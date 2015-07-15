ec808b136188ab8b36a8b41ff3ba9f42f0981a92
Roman Mogilatov <rmogilatov@gmail.com>
2015-07-16 02:39:21 +0300

    Updating callable docs

--- examples/providers/callable.py
+++ examples/providers/callable.py
@@ -1,40 +1,18 @@
-"""`Callable` providers examples."""
+"""`Callable` providers example."""
+
+from passlib.hash import sha256_crypt
 
 from objects.providers import Callable
 from objects.injections import KwArg
 
 
-class SomeCrypt(object):
+# Password hasher and verifier providers (hash function could be changed
+# anytime (for example, to sha512) without any changes in client's code):
+password_hasher = Callable(sha256_crypt.encrypt,
+                           KwArg('salt_size', 16),
+                           KwArg('rounds', 10000))
+password_verifier = Callable(sha256_crypt.verify)
 
-    """Example class SomeCrypt."""
-
-    @staticmethod
-    def encrypt(data, password):
-        """Encypt data using password."""
-        return ''.join((password, data, password))
-
-    @staticmethod
-    def decrypt(data, password):
-        """Decrypt data using password."""
-        return data[len(password):-len(password)]
-
-
-# Encrypt and decrypt function providers:
-encrypt = Callable(SomeCrypt.encrypt,
-                   KwArg('password', 'secret123'))
-decrypt = Callable(SomeCrypt.decrypt,
-                   KwArg('password', 'secret123'))
-
-# Making some asserts:
-initial_data = 'some_data'
-
-encrypted1 = encrypt(initial_data)
-decrypted1 = decrypt(encrypted1)
-
-assert decrypted1 == initial_data
-
-# Context keyword arguments priority example:
-encrypted2 = encrypt(initial_data, password='another_secret')
-decrypted2 = decrypt(encrypted2)
-
-assert decrypted2 != initial_data
+# Making some asserts (client's code):
+hashed_password = password_hasher('super secret')
+assert password_verifier('super secret', hashed_password)
