0972e791302d1311acd8e98279cd55149bfdc52c
Roman Mogilatov <rmogilatov@gmail.com>
2015-07-25 00:12:52 +0300

    Adding new providers overriding examples

---
+++ examples/providers/overriding_simple.py
@@ -0,0 +1,36 @@
+"""Simple providers overriding example."""
+
+from objects.providers import Factory
+
+
+class User(object):
+
+    """Example class User."""
+
+# Users factory:
+users_factory = Factory(User)
+
+# Creating several User objects:
+user1 = users_factory()
+user2 = users_factory()
+
+# Making some asserts:
+assert user1 is not user2
+assert isinstance(user1, User) and isinstance(user2, User)
+
+
+# Overriding users factory:
+class SuperUser(User):
+
+    """Example class SuperUser."""
+
+
+users_factory.override(Factory(SuperUser))
+
+# Creating some more User objects using overridden users factory:
+user3 = users_factory()
+user4 = users_factory()
+
+# Making some asserts:
+assert user4 is not user3
+assert isinstance(user3, SuperUser) and isinstance(user4, SuperUser)
