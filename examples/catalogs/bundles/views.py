6206cdde8d6c18748faf9e472153d5fce8646599
Roman Mogilatov <rmogilatov@gmail.com>
2015-10-19 10:31:21 +0300

    Update catalog bundles example

---
+++ examples/catalogs/bundles/views.py
@@ -0,0 +1,21 @@
+"""Example web views."""
+
+
+class BaseWebView(object):
+    """Example base class of web view."""
+
+    def __init__(self, services):
+        """Initializer.
+
+        :type services: Services
+        :param services: Bundle of service providers
+        """
+        self.services = services
+
+
+class AuthView(BaseWebView):
+    """Example auth web view."""
+
+
+class PhotosView(BaseWebView):
+    """Example photo processing web view."""
