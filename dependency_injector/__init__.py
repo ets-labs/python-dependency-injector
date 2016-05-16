e2044cb5604568b7edfca1b1c8de6e361fcf942e
Roman Mogilatov <rmogilatov@gmail.com>
2016-05-16 11:15:14 +0300

    Bump version to 1.17.0

--- dependency_injector/__init__.py
+++ dependency_injector/__init__.py
@@ -61,7 +61,7 @@ from dependency_injector.errors import (
 from dependency_injector import catalogs
 catalog = catalogs
 
-VERSION = '1.16.8'
+VERSION = '1.17.0'
 """Version number that follows semantic versioning.
 
 :type: str
