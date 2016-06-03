17d8283144a3019cdda7f6a608160fbca98cf8a1
Roman Mogilatov <rmogilatov@gmail.com>
2016-06-03 12:01:09 +0300

    Remove UndefinedProviderError

--- dependency_injector/errors.py
+++ dependency_injector/errors.py
@@ -6,17 +6,3 @@ class Error(Exception):
 
     All dependency injector errors extend this error class.
     """
-
-
-class UndefinedProviderError(Error, AttributeError):
-    """Undefined provider error.
-
-    This error is used when provider could not be defined, for example:
-
-        - provider with certain name could not be defined
-        - catalog's name of the certain provider could not be defined
-        - etc...
-
-    Also this error extends standard :py:class:`AttributeError`. This gives
-    possibility to use it correctly with ``__getattr__()``.
-    """
