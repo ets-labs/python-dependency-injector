3364d6f1b91fd476f6349bf14f56fa02e4ac921b
Roman Mogylatov <rmogilatov@gmail.com>
2017-10-12 21:55:29 -0700

    Add documentation for FactoryAggregate provider

--- examples/providers/factory_aggregate/prototype.py
+++ examples/providers/factory_aggregate/prototype.py
@@ -6,19 +6,12 @@ class FactoryAggregate(object):
 
     def __init__(self, **factories):
         """Initializer."""
-        self._factories = factories
+        self.factories = factories
+
+    def __call__(self, factory_name, *args, **kwargs):
+        """Create object."""
+        return self.factories[factory_name](*args, **kwargs)
 
     def __getattr__(self, factory_name):
-        """Return factory."""
-        if factory_name not in self._factories:
-            raise AttributeError('There is no such factory')
-        return self._factories[factory_name]
-
-    def create(self, factory_name, *args, **kwargs):
-        """Create object."""
-        if factory_name not in self._factories:
-            raise AttributeError('There is no such factory')
-
-        factory = self._factories[factory_name]
-
-        return factory(*args, **kwargs)
+        """Return factory with specified name."""
+        return self.factories[factory_name]
