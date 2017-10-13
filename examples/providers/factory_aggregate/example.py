774c6ff38b9ea893d43aac98b5cad631b35d731d
Roman Mogylatov <rmogilatov@gmail.com>
2017-10-12 21:55:29 -0700

    Add documentation for FactoryAggregate provider

--- examples/providers/factory_aggregate/example.py
+++ examples/providers/factory_aggregate/example.py
@@ -13,15 +13,17 @@ game_factory = providers.FactoryAggregate(chess=providers.Factory(Chess),
 
 if __name__ == '__main__':
     game_type = sys.argv[1].lower()
+    player1 = sys.argv[2].capitalize()
+    player2 = sys.argv[3].capitalize()
 
-    selected_game = game_factory.create(game_type)
+    selected_game = game_factory(game_type, player1, player2)
     selected_game.play()
 
-    # $ python example.py chess
-    # Playing chess
-
-    # $ python example.py checkers
-    # Playing checkers
-
-    # $ python example.py ludo
-    # Playing ludo
+    # $ python example.py chess John Jane
+    # John and Jane are playing chess
+    #
+    # $ python example.py checkers John Jane
+    # John and Jane are playing checkers
+    #
+    # $ python example.py ludo John Jane
+    # John and Jane are playing ludo
--- examples/providers/factory_aggregate/games.py
+++ examples/providers/factory_aggregate/games.py
@@ -1,12 +1,18 @@
-"""Example module."""
+"""Example games module."""
 
 
 class Game(object):
     """Base game class."""
 
+    def __init__(self, player1, player2):
+        """Initializer."""
+        self.player1 = player1
+        self.player2 = player2
+
     def play(self):
         """Play game."""
-        print('Playing {0}'.format(self.__class__.__name__.lower()))
+        print('{0} and {1} are playing {2}'.format(
+            self.player1, self.player2, self.__class__.__name__.lower()))
 
 
 class Chess(Game):
