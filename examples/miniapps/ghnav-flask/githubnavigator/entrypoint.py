4ea27c145b6fe54a47ae4fc2523107d15cb5cab5
Roman Mogylatov <rmogilatov@gmail.com>
2020-07-10 15:20:43 -0400

    Fix auth token reading from env for ghnav-flask

--- examples/miniapps/ghnav-flask/githubnavigator/entrypoint.py
+++ examples/miniapps/ghnav-flask/githubnavigator/entrypoint.py
@@ -5,5 +5,5 @@ from .application import Application
 
 application = Application()
 application.config.from_yaml('config.yml')
-application.config.github.token.from_env('GITHUB_TOKEN')
+application.config.github.auth_token.from_env('GITHUB_TOKEN')
 app = application.app()
