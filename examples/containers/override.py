d2828519b43b88856f3554385156a6cb559cb4ca
Roman Mogylatov <rmogilatov@gmail.com>
2020-08-16 01:10:08 -0400

    Update container overriding docs

---
+++ examples/containers/override.py
@@ -0,0 +1,31 @@
+"""Container overriding example."""
+
+from dependency_injector import containers, providers
+
+
+class Service:
+    ...
+
+
+class ServiceStub:
+    ...
+
+
+class Container(containers.DeclarativeContainer):
+
+    service = providers.Factory(Service)
+
+
+class OverridingContainer(containers.DeclarativeContainer):
+
+    service = providers.Factory(ServiceStub)
+
+
+if __name__ == '__main__':
+    container = Container()
+    overriding_container = OverridingContainer()
+
+    container.override(overriding_container)
+
+    service = container.service()
+    assert isinstance(service, ServiceStub)
