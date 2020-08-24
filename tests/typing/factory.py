ebd00b74e18c94e7b9e88e34195bfe432a3cc583
Roman Mogylatov <rmogilatov@gmail.com>
2020-08-24 17:12:49 -0400

    Add more tests for factory

--- tests/typing/factory.py
+++ tests/typing/factory.py
@@ -6,9 +6,16 @@ class Animal:
 
 
 class Cat(Animal):
-    ...
+
+    @classmethod
+    def create(cls) -> Animal:
+        return cls()
 
 
-provider = providers.Factory(Cat)
+# Test 1: to check the return type (class)
+provider1 = providers.Factory(Cat)
+animal1: Animal = provider1(1, 2, 3, b='1', c=2, e=0.0)
 
-animal: Animal = provider(1, 2, 3, b='1', c=2, e=0.0)
+# Test 2: to check the return type (class factory method)
+provider2 = providers.Factory(Cat.create)
+animal2: Animal = provider2()
