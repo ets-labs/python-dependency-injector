6528411271f49540140ec951953c2beeaf1570c1
Roman Mogylatov <rmogilatov@gmail.com>
2020-09-03 17:00:14 -0400

    Add container usage for object provider example

--- examples/providers/object.py
+++ examples/providers/object.py
@@ -1,10 +1,14 @@
 """`Object` provider example."""
 
-from dependency_injector import providers
+from dependency_injector import containers, providers
 
 
-object_provider = providers.Object(1)
+class Container(containers.DeclarativeContainer):
+
+    object_provider = providers.Object(1)
 
 
 if __name__ == '__main__':
-    assert object_provider() == 1
+    container = Container()
+
+    assert container.object_provider() == 1
