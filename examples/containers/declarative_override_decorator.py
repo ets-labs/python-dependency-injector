f3ca03e569ac042b47694b12c1b72ee527ab9657
Roman Mogylatov <rmogilatov@gmail.com>
2020-10-22 11:41:15 -0400

    Add example and docs

---
+++ examples/containers/declarative_override_decorator.py
@@ -0,0 +1,25 @@
+"""Declarative container provider overriding with `@override()` decorator."""
+
+import sqlite3
+from unittest import mock
+
+from dependency_injector import containers, providers
+
+
+class Container(containers.DeclarativeContainer):
+
+    database = providers.Singleton(sqlite3.connect, ':memory:')
+
+
+# Overriding `Container` with `OverridingContainer`:
+@containers.override(Container)
+class OverridingContainer(containers.DeclarativeContainer):
+
+    database = providers.Singleton(mock.Mock)
+
+
+if __name__ == '__main__':
+    container = Container()
+
+    database = container.database()
+    assert isinstance(database, mock.Mock)
