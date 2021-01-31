fc83d3282cef02d4a958cb03f8ff4276f7a42f9e
Roman Mogylatov <rmogilatov@gmail.com>
2021-01-31 10:10:23 -0500

    Add resource tests

--- tests/unit/providers/test_traversal_py3.py
+++ tests/unit/providers/test_traversal_py3.py
@@ -571,3 +571,57 @@ class DictTests(unittest.TestCase):
         self.assertIn(provider1, all_providers)
         self.assertIn(provider2, all_providers)
         self.assertIn(provider3, all_providers)
+
+
+class ResourceTests(unittest.TestCase):
+
+    def test_traverse(self):
+        provider = providers.Resource(dict)
+        all_providers = list(provider.traverse())
+        self.assertEqual(len(all_providers), 0)
+
+    def test_traverse_args(self):
+        provider1 = providers.Object('bar')
+        provider2 = providers.Object('baz')
+        provider = providers.Resource(list, 'foo', provider1, provider2)
+
+        all_providers = list(provider.traverse())
+
+        self.assertEqual(len(all_providers), 2)
+        self.assertIn(provider1, all_providers)
+        self.assertIn(provider2, all_providers)
+
+    def test_traverse_kwargs(self):
+        provider1 = providers.Object('bar')
+        provider2 = providers.Object('baz')
+        provider = providers.Resource(dict, foo='foo', bar=provider1, baz=provider2)
+
+        all_providers = list(provider.traverse())
+
+        self.assertEqual(len(all_providers), 2)
+        self.assertIn(provider1, all_providers)
+        self.assertIn(provider2, all_providers)
+
+    def test_traverse_overridden(self):
+        provider1 = providers.Resource(list)
+        provider2 = providers.Resource(tuple)
+
+        provider = providers.Resource(dict, 'foo')
+        provider.override(provider1)
+        provider.override(provider2)
+
+        all_providers = list(provider.traverse())
+
+        self.assertEqual(len(all_providers), 2)
+        self.assertIn(provider1, all_providers)
+        self.assertIn(provider2, all_providers)
+
+    def test_traverse_provides(self):
+        provider1 = providers.Callable(list)
+
+        provider = providers.Resource(provider1)
+
+        all_providers = list(provider.traverse())
+
+        self.assertEqual(len(all_providers), 1)
+        self.assertIn(provider1, all_providers)
