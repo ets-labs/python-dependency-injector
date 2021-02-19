a85d89e6f22eb9f89a6820ac8758b756040acf3f
Roman Mogylatov <rmogilatov@gmail.com>
2021-02-19 08:11:05 -0500

    Amend docs and add another example for ``@containers.copy()`` decorator

---
+++ examples/containers/declarative_copy_decorator2.py
@@ -0,0 +1,33 @@
+"""Declarative container provider copying with ``@copy()`` decorator."""
+
+from dependency_injector import containers, providers
+
+
+class Service:
+    def __init__(self, dependency: str):
+        self.dependency = dependency
+
+
+class Base(containers.DeclarativeContainer):
+    dependency = providers.Dependency(instance_of=str, default='Default value')
+    service = providers.Factory(Service, dependency=dependency)
+
+
+@containers.copy(Base)
+class Derived1(Base):
+    dependency = providers.Dependency(instance_of=str, default='Derived 1')
+
+
+# @containers.copy(Base)  # <-- No @copy decorator
+class Derived2(Base):
+    dependency = providers.Dependency(instance_of=str, default='Derived 2')
+
+
+if __name__ == '__main__':
+    container1 = Derived1()
+    service1 = container1.service()
+    print(service1.dependency)  # Derived 1
+
+    container2 = Derived2()
+    service2 = container2.service()
+    print(service2.dependency)  # Default value
