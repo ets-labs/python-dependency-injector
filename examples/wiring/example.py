67d16bf48b2811e0f0c96db69f9d83c7b118d50a
Roman Mogylatov <rmogilatov@gmail.com>
2021-09-27 20:22:15 -0400

    Update main example

--- examples/wiring/example.py
+++ examples/wiring/example.py
@@ -1,7 +1,5 @@
 """Wiring example."""
 
-import sys
-
 from dependency_injector import containers, providers
 from dependency_injector.wiring import inject, Provide
 
@@ -22,6 +20,6 @@ def main(service: Service = Provide[Container.service]) -> None:
 
 if __name__ == '__main__':
     container = Container()
-    container.wire(modules=[sys.modules[__name__]])
+    container.wire(modules=["__main__"])
 
     main()
