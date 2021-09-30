e670377bb3f764758dbe6c221584004f77c8f392
Roman Mogylatov <rmogilatov@gmail.com>
2021-09-30 16:55:50 -0400

    Update quotes in fastapi example

--- examples/miniapps/fastapi/giphynavigator/tests.py
+++ examples/miniapps/fastapi/giphynavigator/tests.py
@@ -11,7 +11,7 @@ from giphynavigator.giphy import GiphyClient
 
 @pytest.fixture
 def client(event_loop):
-    client = AsyncClient(app=app, base_url='http://test')
+    client = AsyncClient(app=app, base_url="http://test")
     yield client
     event_loop.run_until_complete(client.aclose())
 
@@ -20,29 +20,29 @@ def client(event_loop):
 async def test_index(client):
     giphy_client_mock = mock.AsyncMock(spec=GiphyClient)
     giphy_client_mock.search.return_value = {
-        'data': [
-            {'url': 'https://giphy.com/gif1.gif'},
-            {'url': 'https://giphy.com/gif2.gif'},
+        "data": [
+            {"url": "https://giphy.com/gif1.gif"},
+            {"url": "https://giphy.com/gif2.gif"},
         ],
     }
 
     with app.container.giphy_client.override(giphy_client_mock):
         response = await client.get(
-            '/',
+            "/",
             params={
-                'query': 'test',
-                'limit': 10,
+                "query": "test",
+                "limit": 10,
             },
         )
 
     assert response.status_code == 200
     data = response.json()
     assert data == {
-        'query': 'test',
-        'limit': 10,
-        'gifs': [
-            {'url': 'https://giphy.com/gif1.gif'},
-            {'url': 'https://giphy.com/gif2.gif'},
+        "query": "test",
+        "limit": 10,
+        "gifs": [
+            {"url": "https://giphy.com/gif1.gif"},
+            {"url": "https://giphy.com/gif2.gif"},
         ],
     }
 
@@ -51,28 +51,28 @@ async def test_index(client):
 async def test_index_no_data(client):
     giphy_client_mock = mock.AsyncMock(spec=GiphyClient)
     giphy_client_mock.search.return_value = {
-        'data': [],
+        "data": [],
     }
 
     with app.container.giphy_client.override(giphy_client_mock):
-        response = await client.get('/')
+        response = await client.get("/")
 
     assert response.status_code == 200
     data = response.json()
-    assert data['gifs'] == []
+    assert data["gifs"] == []
 
 
 @pytest.mark.asyncio
 async def test_index_default_params(client):
     giphy_client_mock = mock.AsyncMock(spec=GiphyClient)
     giphy_client_mock.search.return_value = {
-        'data': [],
+        "data": [],
     }
 
     with app.container.giphy_client.override(giphy_client_mock):
-        response = await client.get('/')
+        response = await client.get("/")
 
     assert response.status_code == 200
     data = response.json()
-    assert data['query'] == app.container.config.default.query()
-    assert data['limit'] == app.container.config.default.limit()
+    assert data["query"] == app.container.config.default.query()
+    assert data["limit"] == app.container.config.default.limit()
