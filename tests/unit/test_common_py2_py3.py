af286548b3096e03a46fe45adf9ec9acc02758a7
Roman Mogylatov <rmogilatov@gmail.com>
2021-10-12 21:55:05 -0400

    Migrate common tests to pytest

--- tests/unit/test_common_py2_py3.py
+++ tests/unit/test_common_py2_py3.py
@@ -1,11 +1,7 @@
 """Dependency injector common unit tests."""
 
-import unittest
-
 from dependency_injector import __version__
 
 
-class VersionTest(unittest.TestCase):
-
-    def test_version_follows_semantic_versioning(self):
-        self.assertEqual(len(__version__.split(".")), 3)
+def test_version_follows_semantic_versioning():
+    assert len(__version__.split(".")) == 3
