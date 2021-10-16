94ae38635bf8e9ee665caf156c10fe6ad65d6d25
Roman Mogylatov <rmogilatov@gmail.com>
2021-10-16 12:04:08 -0400

    Migrate self provider tests

--- tests/unit/providers/test_base_py2_py3.py
+++ tests/unit/providers/test_base_py2_py3.py
@@ -154,67 +154,6 @@ class ProviderTests(unittest.TestCase):
         )
 
 
-class SelfProviderTests(unittest.TestCase):
-
-    def test_is_provider(self):
-        assert providers.is_provider(providers.Self()) is True
-
-    def test_call_object_provider(self):
-        container = containers.DeclarativeContainer()
-        assert providers.Self(container)() is container
-
-    def test_set_container(self):
-        container = containers.DeclarativeContainer()
-        provider = providers.Self()
-        provider.set_container(container)
-        assert provider() is container
-
-    def test_set_alt_names(self):
-        provider = providers.Self()
-        provider.set_alt_names({"foo", "bar", "baz"})
-        assert set(provider.alt_names) == {"foo", "bar", "baz"}
-
-    def test_deepcopy(self):
-        provider = providers.Self()
-
-        provider_copy = providers.deepcopy(provider)
-
-        assert provider is not provider_copy
-        assert isinstance(provider, providers.Self)
-
-    def test_deepcopy_from_memo(self):
-        provider = providers.Self()
-        provider_copy_memo = providers.Provider()
-
-        provider_copy = providers.deepcopy(
-            provider, memo={id(provider): provider_copy_memo})
-
-        assert provider_copy is provider_copy_memo
-
-    def test_deepcopy_overridden(self):
-        provider = providers.Self()
-        overriding_provider = providers.Provider()
-
-        provider.override(overriding_provider)
-
-        provider_copy = providers.deepcopy(provider)
-        overriding_provider_copy = provider_copy.overridden[0]
-
-        assert provider is not provider_copy
-        assert isinstance(provider, providers.Self)
-
-        assert overriding_provider is not overriding_provider_copy
-        assert isinstance(overriding_provider_copy, providers.Provider)
-
-    def test_repr(self):
-        container = containers.DeclarativeContainer()
-        provider = providers.Self(container)
-        assert repr(provider) == (
-            "<dependency_injector.providers."
-            "Self({0}) at {1}>".format(repr(container), hex(id(provider)))
-        )
-
-
 class DelegateTests(unittest.TestCase):
 
     def setUp(self):
