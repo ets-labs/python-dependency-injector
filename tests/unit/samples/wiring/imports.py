aa11b0bb82d40830c791e89d4225f1578b2add66
VKFisher <vladkf@gmail.com>
2022-03-22 10:36:58 +0700

    import numpy.typing in tests

--- tests/unit/samples/wiring/imports.py
+++ tests/unit/samples/wiring/imports.py
@@ -5,6 +5,7 @@ import sys
 if "pypy" not in sys.version.lower():
     import numpy  # noqa
     from numpy import *  # noqa
+    from numpy.typing import *  # noqa
 
     import scipy  # noqa
     from scipy import *  # noqa
