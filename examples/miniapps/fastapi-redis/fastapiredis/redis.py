93ed0e283b12e4e3b736fa942fcfb7467ae7d3d4
smirnovskoe <smirnovskoe@gmail.com>
2022-07-31 09:55:20 -0700

    Rearranged aioredis.from_url parameters

--- examples/miniapps/fastapi-redis/fastapiredis/redis.py
+++ examples/miniapps/fastapi-redis/fastapiredis/redis.py
@@ -4,7 +4,7 @@ from aioredis import from_url, Redis
 
 
 async def init_redis_pool(host: str, password: str) -> AsyncIterator[Redis]:
-    session = from_url(f"redis://{host}", encoding="utf-8", password=password, decode_responses=True)
+    session = from_url(f"redis://{host}", password=password, encoding="utf-8", decode_responses=True)
     yield session
     session.close()
     await session.wait_closed()
