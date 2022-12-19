f0d9eda56689862c702ba7db13bb841d1e452cfa
Yan <54033129+smirnovskoe@users.noreply.github.com>
2022-12-19 05:27:36 +0300

    Update aioredis to 2.0.1 (#613)
    * Update aioredis to 2.0.1
    
    * Rearranged aioredis.from_url parameters

--- examples/miniapps/fastapi-redis/fastapiredis/redis.py
+++ examples/miniapps/fastapi-redis/fastapiredis/redis.py
@@ -1,12 +1,10 @@
-"""Redis client module."""
-
 from typing import AsyncIterator
 
-from aioredis import create_redis_pool, Redis
+from aioredis import from_url, Redis
 
 
 async def init_redis_pool(host: str, password: str) -> AsyncIterator[Redis]:
-    pool = await create_redis_pool(f"redis://{host}", password=password)
-    yield pool
-    pool.close()
-    await pool.wait_closed()
+    session = from_url(f"redis://{host}", password=password, encoding="utf-8", decode_responses=True)
+    yield session
+    session.close()
+    await session.wait_closed()
