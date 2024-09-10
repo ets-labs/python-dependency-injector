46034cbeb1871b80beacdd70ccb3d1f2cabbe12c
Roman Mogylatov <rmogilatov@gmail.com>
2024-09-09 21:53:37 -0400

    Update the copuright in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Dependency Injector"
-copyright = "2022, Roman Mogylatov"
+copyright = "2024, Roman Mogylatov"
 author = "Roman Mogylatov"
 
 # The version info for the project you"re documenting, acts as replacement for
