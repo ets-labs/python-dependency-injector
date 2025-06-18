e6cc12762f0355f2aff866b08eb1a3427b8b6403
ZipFile <zipfile.d@protonmail.com>
2025-06-18 21:58:00 +0000

    Add support for resource_type in Lifespans

--- tests/unit/ext/test_starlette.py
+++ tests/unit/ext/test_starlette.py
@@ -1,4 +1,4 @@
-from typing import AsyncIterator, Iterator
+from typing import AsyncIterator, Iterator, TypeVar
 from unittest.mock import ANY
 
 from pytest import mark
@@ -6,6 +6,12 @@ from pytest import mark
 from dependency_injector.containers import DeclarativeContainer
 from dependency_injector.ext.starlette import Lifespan
 from dependency_injector.providers import Resource
+
+T = TypeVar("T")
+
+
+class XResource(Resource[T]):
+    """A test provider"""
 
 
 class TestLifespan:
@@ -28,11 +34,15 @@ class TestLifespan:
             yield
             shutdown = True
 
+        def nope():
+            assert False, "should not be called"
+
         class Container(DeclarativeContainer):
-            x = Resource(sync_resource if sync else async_resource)
+            x = XResource(sync_resource if sync else async_resource)
+            y = Resource(nope)
 
         container = Container()
-        lifespan = Lifespan(container)
+        lifespan = Lifespan(container, resource_type=XResource)
 
         async with lifespan(ANY) as scope:
             assert scope is None
