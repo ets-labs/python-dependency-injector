ad9042f4a0e42b9d2cc3100eafcd52a96998f861
Roman Tolkachyov <roman@tolkachyov.name>
2026-01-31 14:56:48 +0300

    add keep_cache argument to Container.wire typings

--- src/dependency_injector/containers.pyi
+++ src/dependency_injector/containers.pyi
@@ -72,6 +72,7 @@ class Container:
         modules: Optional[Iterable[Any]] = None,
         packages: Optional[Iterable[Any]] = None,
         from_package: Optional[str] = None,
+        keep_cache: bool | None = None,
         warn_unresolved: bool = False,
     ) -> None: ...
     def unwire(self) -> None: ...
